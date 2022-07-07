82809b376d8b46d77efedae9c4f05fb89e9fd96b
우성 <32770312+fhdufhdu@users.noreply.github.com>
2022-07-07 22:04:58 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/README.md
+++ 프로그래머스/lv2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://programmers.co.kr/learn/courses/30/lessons/42842?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.00 ms
+메모리: 10.2 MB, 시간: 0.01 ms
 
 ### 구분
 
--- 프로그래머스/lv2/42842. 카펫/카펫.py
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -1,13 +1,13 @@
 def solution(brown, yellow):
     answer = []
     for i in range(1, yellow + 1):
-        j = int(yellow / i)
+        j = int(yellow / i) 
         inner = i * j
         outter = ((i + 2) * (j + 2)) - inner
         if inner != yellow or outter != brown:
             continue
         if i > j:
-            answer.append(i + 2) 
+            answer.append(i + 2)   
             answer.append(j + 2)
         else:
             answer.append(j + 2)
