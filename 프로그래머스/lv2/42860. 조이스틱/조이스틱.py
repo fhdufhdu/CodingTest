0bef8c416665ba18dec909a34f54962905447deb
우성 <32770312+fhdufhdu@users.noreply.github.com>
2022-07-08 16:25:21 +0900

    [level 2] Title: 조이스틱, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42860. 조이스틱/조이스틱.py
@@ -0,0 +1,14 @@
+def solution(name):
+    answer = 0
+    for n in list(name):
+        answer += min(ord(n) - ord('A'), ord('Z') - ord(n) + 1)
+    
+    move_cnt_list = []
+    length = len(name)
+    for i in range(length):
+       	end = i + 1
+        while end < length and name[end] == 'A': end += 1
+        move_cnt_list.append(i * 2 + length - end)
+        move_cnt_list.append((length - end) * 2 + i)
+    answer += min(move_cnt_list)
+    return answer
