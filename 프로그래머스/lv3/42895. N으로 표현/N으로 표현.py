143f05978da27f45f72865173693027c3787c544
우성 <32770312+fhdufhdu@users.noreply.github.com>
2022-07-10 21:33:37 +0900

    [level 3] Title: N으로 표현, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.py
@@ -0,0 +1,31 @@
+def get(N, cnt):
+    result = 0
+    for i in range(cnt):
+       	result = result * 10 + N 
+    return result
+def solution(N, number):
+    create_list = [[N]]
+    answer = 1
+    if N == number:
+        return answer
+    while answer <= 8:
+        answer += 1
+        current_create = [get(N, answer)]
+        for i in range(answer - 1):
+            a = create_list[i]
+            b = create_list[answer - 2 - i]
+            for a_ in a:
+                for b_ in b:
+                    current_create.append(a_ + b_)
+                    current_create.append(a_ * b_)
+                    sub = a_ - b_
+                    if sub > 0:
+                        current_create.append(sub)
+                    div = int(a_ / b_)
+                    if div > 0:
+                        current_create.append(div)
+        create_list.append(current_create)
+        for cur in current_create:
+            if cur == number:
+                return answer
+    return -1
