460a6efa1ce392b7706c74be3c5ab733826840f3
우성 <32770312+fhdufhdu@users.noreply.github.com>
2022-07-10 21:55:35 +0900

    [level 3] Title: 네트워크, Time: 0.24 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -0,0 +1,22 @@
+def solution(n, computers):
+    answer = 0
+    g = [[] for i in range(n)]
+    v = [False for i in range(n)]
+    for i in range(n):
+        for j in range(n):
+            isConn = True if computers[i][j] == 1 else False
+            if not isConn or i == j: continue
+            g[i].append(j)
+    for i in range(n):
+        if v[i]: continue
+        stack = [i]
+        v[i] = True
+        
+        while stack:
+            node = stack.pop()
+            for next_node in g[node]:
+                if v[next_node]: continue
+                stack.append(next_node)
+                v[next_node] = True
+        answer += 1
+    return answer
