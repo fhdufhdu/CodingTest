8e70df3c09de7fe59ab4ea4761901c0620510680
우성 <32770312+fhdufhdu@users.noreply.github.com>
2022-07-12 01:20:41 +0900

    [level 3] Title: 입국심사, Time: 0.03 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.py
@@ -0,0 +1,18 @@
+def solution(n, times):
+    times = sorted(times)
+    min_time = 1
+    max_time = times[-1] * n
+    answer = 1000000000000000
+    while min_time <= max_time:
+        mid_time = (min_time + max_time) // 2
+        available_h = 0
+        for t in times:
+            available_h += (mid_time // t)
+        if available_h >= n:
+            if answer > mid_time:
+                answer = mid_time
+        if available_h < n:
+           	min_time = mid_time + 1
+        else:
+            max_time = mid_time - 1
+    return answer
