54b9ba2eb8ab0214a65337db7f7da207588c50a0
우성 <32770312+fhdufhdu@users.noreply.github.com>
2022-07-12 10:47:51 +0900

    [level 3] Title: 가장 먼 노드, Time: 28.77 ms, Memory: 20.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -0,0 +1,24 @@
+from collections import deque
+def solution(n, edge):
+    answer = 0
+    max_dist = 0
+    g = [[] for _ in range(n+1)]
+    v = [False for _ in range(n+1)]
+    for a, b in edge:
+       	g[a].append(b)
+        g[b].append(a)
+    
+    deq = deque([(1, 0)])
+    v[1] = True
+    while deq:
+        node, dist = deq.popleft()
+        if dist > max_dist:
+            max_dist = dist
+            answer = 1
+        elif dist == max_dist:
+           	answer += 1 
+        for next_node in g[node]:
+            if v[next_node] :continue
+            deq.append((next_node, dist + 1))
+            v[next_node] = True
+    return answer
