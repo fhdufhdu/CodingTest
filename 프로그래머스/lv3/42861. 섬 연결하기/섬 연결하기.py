40b84932a091191c68fac7b46319fb696a0a6b39
우성 <32770312+fhdufhdu@users.noreply.github.com>
2022-08-17 17:38:49 +0900

    [level 3] Title: 섬 연결하기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.py
@@ -0,0 +1,26 @@
+def solution(n, costs):
+    answer = 0
+    #최소신장트리?
+    data = [i for i in range(101)]
+    costs = sorted(costs, key=lambda x:x[2])
+    
+    def find(a):
+        prev = []
+       	while a != data[a]:
+            prev.append(a) 
+            a = data[a]
+        for p in prev:
+            data[p] = a
+        return a
+    def union(a, b):
+        ar = find(a)
+        br = find(b)
+        data[ar] = br
+    
+    for a, b, cost in costs:
+        if find(a) == find(b):
+            continue
+        union(a, b)
+        answer += cost
+        
+    return answer
