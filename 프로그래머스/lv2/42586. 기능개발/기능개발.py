ce367e46fa9fba191aabbb33e78045edb967ad73
fhdufhdu <32770312+fhdufhdu@users.noreply.github.com>
2022-08-24 19:17:51 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -0,0 +1,18 @@
+def solution(progresses, speeds): 
+    answer = []
+    progresses = list(reversed(progresses))
+    speeds = list(reversed(speeds))
+    
+    while progresses:
+        progresses = [x + y for x, y in zip(progresses, speeds)] 
+        pop_count = 0
+        while progresses:
+            top = progresses[-1]
+            if top >= 100:
+                progresses.pop()
+                pop_count += 1
+                continue
+            break
+       	if pop_count != 0:
+            answer.append(pop_count) 
+    return answer
