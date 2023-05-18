964a985e4fe3f1bf544ae5ca29b4539d8e727388
lucas.ws <32770312+fhdufhdu@users.noreply.github.com>
2023-05-18 12:23:49 +0900

    [Gold III] Title: 소수의 연속합, Time: 488 ms, Memory: 166264 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
@@ -3,37 +3,38 @@ from sys import stdin, stdout
 read = stdin.readline
 write = stdout.write
 
-max = 4000001
-p = [True] * max
-p[0] = False
-p[1] = False
+n = int(read().rstrip())
 
-for i in range(2, int((max - 1) ** 0.5) + 1):
-    if p[i] == True:
-        idx = i ** 2
-        while idx <= max-1:
-            p[idx] = False
-            idx += i
+prime_net = [True] * 4000001
+prime_net[0] = False
+prime_net[1] = False
 
-n = int(read())
-p_list = []
-for i in range(1, n+1):
-    if p[i] == True:
-        p_list.append(i)
+prime_list = []
+for i in range(2, int(4000000 ** 0.5) + 1):
+    if not prime_net[i]: continue
+    ni = i ** 2
+    while ni < 4000001:
+        prime_net[ni] = False
+        ni += i
 
-p_list.append(0)
+for i in range(2, n+1):
+    if prime_net[i]:
+        prime_list.append(i)
 
-p1=p2=0
-s = 0
+prime_list.append(0)
+
+p1 = p2 = 0
 cnt = 0
-while p1 < len(p_list):
-    if s <= n:
-        if s == n:
+sumv = 0
+
+while p1 < len(prime_list):
+    if sumv <= n:
+        if sumv == n:
             cnt += 1
-        s += p_list[p1]
+        sumv += prime_list[p1]
         p1 += 1
     else:
-        s -= p_list[p2]
+        sumv -= prime_list[p2]
         p2 += 1
 
-write(f'{cnt}\n')
+write(str(cnt))
