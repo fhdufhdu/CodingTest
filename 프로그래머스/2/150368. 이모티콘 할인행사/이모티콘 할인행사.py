e963101bb8741462e3b65ba5fea1f96f7d0ece2c
lucas.ws <32770312+fhdufhdu@users.noreply.github.com>
2023-10-13 10:38:34 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 0.07 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.py
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.py
@@ -1,33 +1,34 @@
-answer = [0, 0]
-def solution(users, emoticons):
-    global answer
-    emoticon_len = len(emoticons)
-    sale_per = [None] * emoticon_len
-    def dfs(cnt:int, sale_per:list):
-        global answer
-        cnt = cnt + 1
-        if cnt >= emoticon_len:
-            temp_answer = [0, 0]
+answer = [0, 0]	
+sale_pers = None
+
+        
+def solution(users, emoticons): 
+    global sale_pers
+    
+    def dfs(n, max_n):
+        global answer, sale_pers
+        n += 1
+        if n >= max_n:
+            temp_ans = [0, 0]
             for per, price in users:
                 sum_price = 0
                 for idx, emo_price in enumerate(emoticons):
-                    if sale_per[idx] >= per:
-                        sum_price += emo_price * (1-(sale_per[idx]/100))   
+                    if sale_pers[idx] < per: continue
+                    sum_price += emo_price * (1 - sale_pers[idx]/100)
                 if sum_price >= price:
-                    temp_answer[0] += 1
+                    temp_ans[0] += 1
                 else:
-                    temp_answer[1] += sum_price
-            if answer[0] < temp_answer[0]:
-                answer = temp_answer
-            elif(
-                answer[0] == temp_answer[0]
-                and answer[1] < temp_answer[1]
-            ):
-                answer = temp_answer
+                    temp_ans[1] += sum_price
+
+            answer = max(answer, temp_ans, key=lambda x:(x[0], x[1]))
+            
             return
         for per in (10, 20, 30, 40):
-            sale_per[cnt] = per
-            dfs(cnt, sale_per)
-    dfs(-1, sale_per)
-            
+            sale_pers[n] = per
+            dfs(n, max_n)
+    
+    emo_len = len(emoticons)
+    sale_pers = [None] * emo_len
+    dfs(-1, emo_len)
+     
     return answer
