de35a118cd1ed2c888c0a5355bff6de270b87b12
lucas.ws <32770312+fhdufhdu@users.noreply.github.com>
2024-02-28 13:52:10 +0900

    [level 2] Title: 요격 시스템, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.py
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.py
@@ -1,4 +1,10 @@
 def solution(targets):
+    """
+    1. targets을 정렬
+    2. 각 target 순회하면서 겹치는 부분 계속 갱신하기
+    3. 겹치는 부분없으면 answer+1하고 새롭게 겹치는 부분 생성
+    4. 전체 겹치는 부분의 갯수가 정답
+   	"""
     targets = sorted(targets)
      
     _ranges = [targets[0]]
