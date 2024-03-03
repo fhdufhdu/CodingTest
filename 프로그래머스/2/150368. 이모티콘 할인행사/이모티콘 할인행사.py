b0c8be544b3b02e8b58438ab8fa09676dd865a2c
lucas.ws <32770312+fhdufhdu@users.noreply.github.com>
2024-03-03 13:58:06 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 0.05 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.py
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.py
@@ -1,34 +1,29 @@
-answer = [0, 0]	
-sale_pers = None
+import itertools
 
-        
-def solution(users, emoticons): 
-    global sale_pers
-    
-    def dfs(n, max_n):
-        global answer, sale_pers
-        n += 1
-        if n >= max_n:
-            temp_ans = [0, 0]
-            for per, price in users:
-                sum_price = 0
-                for idx, emo_price in enumerate(emoticons):
-                    if sale_pers[idx] < per: continue
-                    sum_price += emo_price * (1 - sale_pers[idx]/100)
-                if sum_price >= price:
-                    temp_ans[0] += 1
-                else:
-                    temp_ans[1] += sum_price
-
-            answer = max(answer, temp_ans, key=lambda x:(x[0], x[1]))
+def solution(users, emoticons):
+    """
+    할인율을 중복 허용해서 이모티콘 갯수만큼 뽑음
+    만약 할인율이 [1, 2]이고 이모티콘 갯수가 3개라면
+    2 * 2 * 2 = 8개의 경우의 수가 나옴 이걸 모두 계산하는거임
+    """
+    cases = itertools.product([10, 20, 30, 40], repeat=len(emoticons))
+    cases = list(cases)
+   	
+    answer = (0, 0)
+    for case in cases:
+        plus_user_cnt = 0
+        total_emoticon_price = 0
+        for want_percent, max_price in users: 
+            user_price_total = 0 
+            for sale_percent, emoticon_price in zip(case, emoticons):
+                if sale_percent >= want_percent:
+                    user_price_total += emoticon_price * (1-(sale_percent/100))
+            if user_price_total >= max_price:
+                plus_user_cnt += 1
+            else:
+                total_emoticon_price += user_price_total
+        result = (plus_user_cnt, total_emoticon_price)
+        if answer < result:
+            answer = result 
             
-            return
-        for per in (10, 20, 30, 40):
-            sale_pers[n] = per
-            dfs(n, max_n)
-    
-    emo_len = len(emoticons)
-    sale_pers = [None] * emo_len
-    dfs(-1, emo_len)
-     
-    return answer
+    return list(answer)
