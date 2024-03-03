c17b8c55dd7593788cdd3bb49044cd4d19af0fb5
lucas.ws <32770312+fhdufhdu@users.noreply.github.com>
2024-03-03 12:41:05 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 48.62 ms, Memory: 19.3 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -1,29 +1,27 @@
 from collections import deque
 def solution(queue1, queue2):
     answer = 0
-    max_answer = (len(queue1) + len(queue2)) * 2
-    
-    target = sum(queue1) + sum(queue2)
-    if target % 2 == 1: 
-        return -1
-    
-    target = target // 2
-   	
-    q1s = sum(queue1)
-    
     queue1 = deque(queue1)
     queue2 = deque(queue2)
-    while target != q1s:
-        if q1s > target:
-            qp = queue1.popleft() 
-            q1s -= qp
-            queue2.append(qp)
-        else:
-            qp = queue2.popleft()
-            q1s += qp
-            queue1.append(qp)
-        answer += 1
-        
-        if answer > max_answer:
+    
+    q1_total = sum(queue1)
+    q2_total = sum(queue2)
+    
+    MAX_CNT = 600000
+    
+    while q1_total != q2_total:
+        if answer >= MAX_CNT:
             return -1
+        if q1_total > q2_total:
+            data = queue1.popleft()
+            queue2.append(data)
+            q1_total -= data
+            q2_total += data
+        else:
+            data = queue2.popleft()
+            queue1.append(data)
+            q2_total -= data
+            q1_total += data
+        answer += 1
+            
     return answer
