ca84a760dbf62df6d7d59356c9948d080ac7446d
lucas.ws <32770312+fhdufhdu@users.noreply.github.com>
2024-03-04 19:52:57 +0900

    [level 3] Title: 네트워크, Time: 1.32 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -14,6 +14,16 @@ def union(data, a, b):
     a_root = find(data, a)
     b_root = find(data, b)
     
+    """
+    0 1 2라는 집합이 있을때
+    1번째, 2번째를 병합하면
+    0 1 1이 됨
+    여기서 0번째, 2번째를 병합하면
+    0 0 1이 됨
+    근데 실제로는
+    0 0 0이 되어야함
+    그래서 아래와 같은 처리를 거치는 것
+    """
     for i in range(len(data)):
         if data[i] == b_root:
             data[i] = a_root
