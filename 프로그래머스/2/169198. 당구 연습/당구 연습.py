3552404808dbebcbe9a4b09b7fdd3caa56ed05d0
lucas.ws <32770312+fhdufhdu@users.noreply.github.com>
2024-03-12 17:19:54 +0900

    [level 2] Title: 당구 연습, Time: 3.62 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/169198. 당구 연습/당구 연습.py
+++ 프로그래머스/2/169198. 당구 연습/당구 연습.py
@@ -7,6 +7,11 @@ def solution(m, n, startX, startY, balls):
         for rx, ry in reflect_balls:
             distance = abs(x-rx)**2 + abs(y-ry)**2
             start_ball_distance = abs(startX-rx)**2 + abs(startY-ry)**2
+            """
+            1. 시작 위치, 대칭 위치, 공의 위치가 일직선 상에 없어야함
+            2. (대칭 위치와 공의 위치의 사이의 거리) > (시작 위치와 대칭 위치의 거리)
+               반대라면 쿠션전에 만날 수도 있음
+            """
             if not(startX == x == rx or startY == y ==ry) or start_ball_distance < distance: 
                 ans = min(distance, ans)
         answer.append(ans)
