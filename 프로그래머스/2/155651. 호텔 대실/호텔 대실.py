c9764e6e9091555e2a7007a4bdb669faa2e35931
lucas.ws <32770312+fhdufhdu@users.noreply.github.com>
2024-03-13 15:42:16 +0900

    [level 2] Title: 호텔 대실, Time: 10.52 ms, Memory: 10.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+def solution(book_time):
+    answer = 0
+    b_t = []
+   	
+    for start_time, end_time in book_time:
+        start_time = list(map(int, start_time.split(':')))
+        end_time = list(map(int, end_time.split(':')))
+        start_time = start_time[0] * 60 + start_time[1]
+        end_time = end_time[0] * 60 + end_time[1] + 10
+        b_t.append((start_time, end_time))
+    
+    b_t = sorted(b_t)
+   	
+    counter = defaultdict(int)
+    
+    for st, et in b_t:
+        for t in range(st, et):
+            counter[t] += 1
+    
+    answer = max(counter.values())
+    
+    return answer
