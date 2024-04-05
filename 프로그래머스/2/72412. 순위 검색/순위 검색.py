e4eff9d6d4a11e5c89a3bc6aeaf9954c18bebb7d
lucas.ws <32770312+fhdufhdu@users.noreply.github.com>
2024-04-05 16:29:44 +0900

    [level 2] Title: 순위 검색, Time: 776.64 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/2/72412. 순위 검색/순위 검색.py
+++ 프로그래머스/2/72412. 순위 검색/순위 검색.py
@@ -1,65 +1,50 @@
-from bisect import bisect_left
+from collections import defaultdict
+from itertools import product
+from bisect import bisect_left, bisect_right
 
+language = {
+    'cpp':'0',
+    'java': '1',
+    'python':'2',
+    '-': '-'
+}
+position = {
+    'backend':'0',
+    'frontend':'1',
+    '-': '-'
+}
+career = {
+    'junior':'0',
+    'senior':'1',
+    '-': '-'
+}
+soulfood = {
+    'chicken':'0',
+    'pizza':'1',
+    '-': '-'
+}
 def solution(info, query):
-    a = {"java":"0", "cpp":"1", "python":"2"}
-    b = {"backend":"0", "frontend":"1"}
-    c = {"junior":"0", "senior":"1"}
-    d = {"chicken":"0", "pizza":"1"}
-    
-    results = []
-    for i in info:
-        lang, posi, car, food, score = i.split(" ")
-        results.append([a[lang], b[posi], c[car], d[food], int(score)])
-    
-    r = {}
-    for result in results:
-        _d = "".join(result[:4])
-        if (r.get(_d)):
-            r[_d].append(result[4])
-        else:
-            r[_d] = [result[4]];
-
     answer = []
-    s_dp = {}
-    for __i in r.keys():
-        s_dp[__i] = False
+    
+    info = list(map(lambda x: x.split(' '), info))
+    def query_split(query):
+        cond, score = query.rsplit(' ', 1)
+        cond = cond.split(' and ')
         
-    for q in query:
-        lang, posi, car, f = q.split(" and ")
-        food, score = f.split(" ")
-        u1 = []; u2 = []; u3 = []; u4 = []
-        if (lang == "-"):
-            u1.append("0"); u1.append("1"); u1.append("2");
-        else:
-            u1.append(a[lang])
-        
-        if (posi == "-"):
-            u2.append("0"); u2.append("1");
-        else:
-            u2.append(b[posi])
-        
-        if (car == "-"):
-            u3.append("0"); u3.append("1");
-        else:
-            u3.append(c[car])
-        
-        if (food == "-"):
-            u4.append("0"); u4.append("1");
-        else:
-            u4.append(d[food])
-        
-        count = 0
-        for _u1 in u1:
-            for _u2 in u2:
-                for _u3 in u3:
-                    for _u4 in u4:
-                        s = _u1 +_u2 + _u3 + _u4
-                        if r.get(s):
-                            arr = r[s]
-                            if not s_dp[s]:
-                                arr.sort()
-                                s_dp[s] = True
-                            f = bisect_left(arr,int(score))
-                            count += len(arr) - f
-        answer.append(count)
+        return cond, score
+    query = list(map(query_split, query))
+    
+    data = defaultdict(list)
+    for l, p, c, s, score in info:
+        keys = list(product([language[l], '-'],[position[p], '-'],[career[c], '-'],[soulfood[s], '-']))
+        keys = list(map(lambda x:"".join(x), keys))
+        for key in keys:
+            data[key].append(int(score))
+    
+    for key, value in data.items():
+        data[key] = sorted(value)
+   	
+    for (l, p, c, s), score in query:
+        key = language[l] + position[p] + career[c] + soulfood[s]
+        answer.append(len(data[key]) - bisect_left(data[key], int(score)))
     return answer
