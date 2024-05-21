508abaea1b4d1c3d27d7ba4346c21455163cd1e3
lucas.ws <32770312+fhdufhdu@users.noreply.github.com>
2024-05-21 11:17:13 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.py
@@ -0,0 +1,24 @@
+def gcd(a, b):
+    if a > b:
+        temp = a
+        a = b
+        b = temp
+        
+    c = a % b
+    while c != 0:
+        a = b
+        b = c
+        c = a % b
+    return b
+
+def solution(w,h):
+    total = w * h
+    gcd_value = gcd(w, h)
+    
+    w = w // gcd_value
+    h = h // gcd_value
+    
+    answer = total - ((w + h - 1) * gcd_value)
+    return answer
+
+
