1bb37a21bd25f96b1a7c46a29343b261852d4937
조우성 <32770312+fhdufhdu@users.noreply.github.com>
2024-08-10 15:23:37 +0900

    [level 2] Title: 소수 찾기, Time: 4026.58 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,23 +1,21 @@
-from itertools import permutations, combinations
+from itertools import permutations
 def solution(numbers):
-    net = [True for i in range(10000000)]
+    answer = 0
+    net = [True] * 10000000
     net[0] = False
     net[1] = False
     for i in range(2, 10000000):
-       	if net[i] == True:
-            
-            j = i ** 2
-            while j < 10000000:
-                net[j] = False
-                j += i
-    
-    perm = []
+        if not net[i]: continue 
+        ni = i * 2 
+        while (ni < 10000000):
+            net[ni] = False
+            ni = ni + i
+    result = set()
     for i in range(len(numbers)):
-        perm += list(permutations(numbers, i + 1))
-    available_numbers = set(map(lambda x:int("".join(x)),perm))
+        cases = set(permutations(list(numbers), i+1))
+        for case in cases:
+            case = int("".join(case))
+            if net[case]:
+                result.add(case)
     
-    answer = 0
-    for number in available_numbers:
-        if net[number] == True:
-            answer += 1
-    return answer
+    return len(result)
