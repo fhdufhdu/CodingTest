d208262f416c530ca4182d3f2370891d2284c078
조우성 <32770312+fhdufhdu@users.noreply.github.com>
2024-08-11 18:37:25 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 4.26 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,33 +1,25 @@
 def solution(n, wires):
     answer = 10e9
-    matrix = [[[] for i in range(n+1)] for _ in range(n-1)]
-    visited = [[False for i in range(n+1)] for _ in range(n-1)]
-    
-    for i in range(n-1):
-        for j in range(n-1):
-            if i == j: continue
-            a, b = wires[j]
-            matrix[i][a].append(b)
-            matrix[i][b].append(a)
-   	
-    for i, g in enumerate(matrix):
-        counts = []
-        for j in range(1, n+1):
-            if visited[i][j]: continue
-            
-            count = 1
-            if g[j]:
-                stack = [j]
-                visited[i][j] = True
-                while stack:
-                    cnode = stack.pop()
-                    for nnode in g[cnode]:
-                        if visited[i][nnode]: continue
-
-                        stack.append(nnode)
-                        count += 1
-                        visited[i][nnode] = True
-            counts.append(count)
-        answer = min(answer, abs(counts[0]-counts[1]))
-    
+    g = [[] for _ in range(n+1)]
+    for a, b in wires:
+        g[a].append(b)
+        g[b].append(a)
+    for a, b in wires:
+        visit = [False] * (n + 1)
+        cnts = []
+        for i in range(1, n + 1):
+            if visit[i]: continue
+            stack = [i]
+            visit[i] = True
+            cnt = 1
+            while stack:
+                curr = stack.pop()
+                for _next in g[curr]:
+                    if visit[_next]: continue
+                    if (curr, _next) in [(a, b), (b, a)]: continue
+                    stack.append(_next)
+                    visit[_next] = True
+                    cnt += 1
+            cnts.append(cnt)
+        answer = min(answer, abs(cnts[0]-cnts[1]))
     return answer
