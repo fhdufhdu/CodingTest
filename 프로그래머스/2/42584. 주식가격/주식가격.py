fcf598a704352ef244a3e97a979c629bd2f5cd19
조우성 <32770312+fhdufhdu@users.noreply.github.com>
2024-08-17 15:00:14 +0900

    [level 2] Title: 주식가격, Time: 147.37 ms, Memory: 19.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -0,0 +1,9 @@
+def solution(prices):
+    answer = []
+    for i in range(len(prices)):
+        count = 0
+        for j in range(i+1, len(prices)):
+            count += 1
+            if prices[i] > prices[j]: break
+        answer.append(count)     
+    return answer
