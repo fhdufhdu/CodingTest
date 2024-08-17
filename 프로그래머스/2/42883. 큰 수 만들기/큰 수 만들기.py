068afc98c984f349f1c89fcb46734af1a37725ac
조우성 <32770312+fhdufhdu@users.noreply.github.com>
2024-08-17 16:20:39 +0900

    [level 2] Title: 큰 수 만들기, Time: 102.75 ms, Memory: 13 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,21 +1,15 @@
 def solution(number, k):
     answer = ''
-   	
     temp = []
-    delete_count = 0
     for i in range(len(number)):
-        curr_char = number[i]
-        while temp:
-            prev_char = temp[-1]
-            if prev_char < curr_char and delete_count < k:
-               	delete_count += 1 
+        curr = number[i]
+        while temp and k > 0:
+            if temp[-1] < curr:
                 temp.pop()
+                k -= 1
                 continue
-            break
-       	temp.append(curr_char)
-        
-    while delete_count < k:
+            break 
+        temp.append(curr)
+    for _ in range(k):
         temp.pop()
-        delete_count += 1
     return "".join(temp)
-                
