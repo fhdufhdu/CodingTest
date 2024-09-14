37f3ed473b069bf846d28b46cfa7163cef453794
조우성 <32770312+fhdufhdu@users.noreply.github.com>
2024-09-14 18:01:33 +0900

    [level 2] Title: 피보나치 수, Time: 23.20 ms, Memory: 14 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -2,9 +2,9 @@ def fibo(n):
     f = [0, 1]
     i = 2
     for i in range(i, n+1):
-        f.append(f[i-1] + f[i-2])
+        f.append((f[i-1] + f[i-2]) % 1234567)
     return f[-1]
     
 def solution(n):
-    answer = fibo(n) % 1234567
+    answer = fibo(n)
     return answer
