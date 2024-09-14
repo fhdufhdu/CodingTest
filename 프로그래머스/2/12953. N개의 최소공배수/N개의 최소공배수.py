e94201995ffb989555023a46ca59ec2fca246017
조우성 <32770312+fhdufhdu@users.noreply.github.com>
2024-09-14 17:02:11 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.py
@@ -0,0 +1,13 @@
+def gcd(a, b):
+    if b == 0:
+        return a
+    return gcd(b, a % b)
+def solution(arr):
+    answer = 0
+    while len(arr) > 1:
+        if len(arr) >= 2:
+            a = arr.pop()
+            b = arr.pop()
+            lcm = (a * b) // gcd(a, b)
+            arr.append(lcm)
+    return arr[0]
